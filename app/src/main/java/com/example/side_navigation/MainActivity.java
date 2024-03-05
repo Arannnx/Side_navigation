a5e354a8e5535836a39b371808fd587fa66a3b08
Arannnx <gitallancorpuz@gmail.com>
2024-03-05 17:11:55 +0800

    first commit

--- app/src/main/java/com/example/side_navigation/MainActivity.java
+++ app/src/main/java/com/example/side_navigation/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.side_navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        findViewById(R.id.imageview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { drawerLayout.openDrawer(GravityCompat.START);}
+
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setItemIconTintList(null);
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 }
